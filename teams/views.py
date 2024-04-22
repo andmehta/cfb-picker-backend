76214a6fb9ff16b42dae9f9a6f3d4e4c0ae79c75
andrewmehta <andmehta@icloud.com>
2024-04-22 17:31:29 -0500

    add DRF endpoint for teams

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from teams.models import Team
+from teams.serializers import TeamSerializer
 
-# Create your views here.
+
+class TeamsView(viewsets.ReadOnlyModelViewSet):
+    """This is setup to run .as_view({"get": "list"}) so the HTTP method GET is already defined in DRF
+    POST is defined as NOT ALLOWED since it's the ReadOnlyModelViewSet"""
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
