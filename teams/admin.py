9329dbf3ecd6d14023fcec70d2fc0f39ef975817
andrewmehta <andmehta@icloud.com>
2024-04-22 16:51:02 -0500

    add Teams app and an admin page for adding teams

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from teams.models import Team
 
-# Register your models here.
+
+@admin.register(Team)
+class TeamsAdmin(admin.ModelAdmin):
+    model = Team
+    list_display = ("name", "nickname", "mascot")
+    search_fields = ("name",)
