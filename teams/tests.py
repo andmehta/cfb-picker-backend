7565c3761c05fe9e7da5b0a19d749d67a94fb0b2
andrewmehta <andmehta@icloud.com>
2024-04-24 15:58:18 -0500

    add first unit test

--- teams/tests.py
+++ teams/tests.py
@@ -1,3 +1,20 @@
+from django.core.management import call_command
 from django.test import TestCase
 
-# Create your tests here.
+
+
+class TeamsViewTestCase(TestCase):
+
+    def setUp(self):
+        """First set up Teams data in the database with a fixture"""
+        call_command("loaddata", "initial_teams_data")
+
+    def test_view_with_objects__list_returned(self):
+        """first add objects to the DB, then get the endpoint and see a list"""
+        response = self.client.get("/teams/")
+        if not response.status_code == 200:
+            self.fail(f"status code did not equal 200 actual = {response.status_code}")
+        first_obj = response.data[0]
+        expected_keys = ("name", "nickname", "id", "abbreviation", "mascot", "conference", "primary_color", "secondary_color")
+        for key in first_obj.keys():
+            self.assertTrue(key in expected_keys)
