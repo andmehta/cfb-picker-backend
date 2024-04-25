5bce9f21a03cf3014fd34ebe1a4aaba1da866297
andrewmehta <andmehta@icloud.com>
2024-04-25 08:47:37 -0500

    you can just make a fixture with dumpdata... kinda embarassing I didn't do that first

--- teams/tests/tests_views.py
+++ teams/tests/tests_views.py
@@ -2,8 +2,7 @@ from django.core.management import call_command
 from django.test import TestCase
 
 
-
-class TeamsViewTestCase(TestCase):
+class TestTeamsViewTestCase(TestCase):
 
     def setUp(self):
         """First set up Teams data in the database with a fixture"""
@@ -15,6 +14,7 @@ class TeamsViewTestCase(TestCase):
         if not response.status_code == 200:
             self.fail(f"status code did not equal 200 actual = {response.status_code}")
         first_obj = response.data[0]
-        expected_keys = ("name", "nickname", "id", "abbreviation", "mascot", "conference", "primary_color", "secondary_color")
+        expected_keys = (
+        "name", "nickname", "id", "abbreviation", "mascot", "conference", "primary_color", "secondary_color")
         for key in first_obj.keys():
             self.assertTrue(key in expected_keys)
