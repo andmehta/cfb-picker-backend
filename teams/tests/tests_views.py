9bd722d6b086e5d22dbe369f7d08a50b4abbfbf3
andrewmehta <andmehta@icloud.com>
2024-04-27 17:54:50 -0500

    more tests for teams

--- teams/tests/tests_views.py
+++ teams/tests/tests_views.py
@@ -8,13 +8,26 @@ class TestTeamsViewTestCase(TestCase):
         """First set up Teams data in the database with a fixture"""
         call_command("loaddata", "initial_teams_data")
 
-    def test_view_with_objects__list_returned(self):
+    def test_list_view_with_objects__list_returned(self):
         """first add objects to the DB, then get the endpoint and see a list"""
-        response = self.client.get("/teams/")
+        response = self.client.get("/api/v1/teams/")
         if not response.status_code == 200:
             self.fail(f"status code did not equal 200 actual = {response.status_code}")
+        self.assertIsInstance(response.data, list)
         first_obj = response.data[0]
+        self.assertIsInstance(first_obj, dict)
         expected_keys = (
-        "name", "nickname", "id", "abbreviation", "mascot", "conference", "primary_color", "secondary_color")
+            "name", "nickname", "id", "abbreviation", "mascot", "conference", "primary_color", "secondary_color")
         for key in first_obj.keys():
+            self.assertTrue(key in expected_keys)
+
+    def test_retrieve_view_with_objects__object_returned(self):
+        """See we can get an individual team as well"""
+        response = self.client.get("/api/v1/teams/4/")  # include a PK
+        if not response.status_code == 200:
+            self.fail(f"status code did not equal 200 actual = {response.status_code}")
+        self.assertIsInstance(response.data, dict)
+        expected_keys = (
+            "name", "nickname", "id", "abbreviation", "mascot", "conference", "primary_color", "secondary_color")
+        for key in response.data.keys():
             self.assertTrue(key in expected_keys)
--- teams/urls.py
+++ teams/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from teams.views import TeamsView
 
 urlpatterns = [
-    path("", TeamsView.as_view({"get": "list"}), name="index"),
+    path("", TeamsView.as_view({"get": "list"}), name="teams_list"),
+    path("<int:pk>/", TeamsView.as_view({"get": "retrieve"}), name="teams_retrieve"),
 ]
