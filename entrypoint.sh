5facfca6a9decad7b4bf21ece42dea98c421a579
andrewmehta <andmehta@icloud.com>
2024-05-02 14:44:18 -0500

    better handling in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "migrating database"
-python manage.py migrate
+python manage.py migrate || exit 1
 
+echo "trying to create the superuser"
 # password comes from environment variable DJANGO_SUPERUSER_PASSWORD
-#echo "trying to create the superuser"
-#python manage.py createsuperuser --noinput --username admin1 --email admin@example.com
+# try to create the superuser, but if that fails, just move on
+python manage.py createsuperuser --noinput --username admin1 --email admin@example.com || true
 
 # TODO replace this with a real webserver eventually
 python manage.py runserver 0.0.0.0:8000
