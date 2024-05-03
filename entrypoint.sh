17d36479019e4df88affbdc650f5d77c453e52a3
andrewmehta <andmehta@icloud.com>
2024-05-02 21:10:51 -0500

    load initial data into SQL on entrypoint. These DO NOT break if re-ran. I tested

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,9 @@ echo "trying to create the superuser"
 # try to create the superuser, but if that fails, just move on
 python manage.py createsuperuser --noinput --username admin1 --email admin@example.com || true
 
+python manage.py loaddata initial_teams_data
+
+python manage.py loaddata 2024_season
+
 # TODO replace this with a real webserver eventually
 python manage.py runserver 0.0.0.0:8000
